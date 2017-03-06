791c0dd58384cc49b406c4490a441ff35ad107c8
Zafira Candra <zafiracandrawulan@gmail.com>
2017-03-06 08:10:04 +0700

    Tabs Change Fragment

--- app/src/main/java/id/sch/smktelkom_mlg/learn/navigation2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/navigation2/MainActivity.java
@@ -133,9 +133,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            if (position == 1)
+                return new Page2Fragment();
+            else
+                return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
